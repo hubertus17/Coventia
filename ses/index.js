879aa1cb4d95c9640b3a51805fcf892a9b4d6329
hubert <h.kopysc@gmail.com>
2019-09-28 18:21:04 +0200

    Lambda ses content

--- ses/index.js
+++ ses/index.js
@@ -1,43 +1,76 @@
-// Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
-// Set the region
-AWS.config.update({region: 'eu-west-1'});
 
-// Create sendEmail params
-var params = {
-    Destination: { /* required */
-        ToAddresses: [
-            'kubakunc@gmail.com'
-            /* more items */
-        ]
-    },
-    Message: { /* required */
-        Body: { /* required */
-            Html: {
-                Charset: "UTF-8",
-                Data: "HTML_FORMAT_BODY"
+const fs = require('fs');
+const util = require('util');
+
+async function readFile(path) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, 'utf8', function (err, data) {
+            if (err) {
+                reject(err);
+            }
+            resolve(data);
+        });
+    });
+}
+
+
+exports.handler = async (event) => {
+    console.info("got event:", event);
+
+    const body = JSON.parse(event.body)
+
+    const raw_html = await readFile('./index.html');
+
+    const html = util.format(raw_html, body.userName);
+
+    console.info(html);
+
+    // Create sendEmail params 
+    var params = {
+        Destination: { /* required */
+            ToAddresses: [
+                'h.kopysc@gmail.com'
+                /* more items */
+            ]
+        },
+        Message: { /* required */
+            Body: { /* required */
+                Html: {
+                    Charset: "UTF-8",
+                    Data: html
+                }
             },
-            Text: {
-                Charset: "UTF-8",
-                Data: "Chuju muju kurwaaaaa"
+            Subject: {
+                Charset: 'UTF-8',
+                Data: 'Test email'
             }
         },
-        Subject: {
-            Charset: 'UTF-8',
-            Data: 'Test email'
-        }
-    },
-    Source: 'coventia.test@gmail.com', /* required */
-};
+        Source: 'coventia.test@gmail.com',
+        /* required */
+    };
 
-// Create the promise and SES service object
-var sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
+    // Create the promise and SES service object
+    let ses = new AWS.SES({ apiVersion: '2010-12-01' });
 
-// Handle promise's fulfilled/rejected states
-sendPromise.then(
-    function(data) {
-        console.log(data.MessageId);
-    }).catch(
-    function(err) {
-        console.error(err, err.stack);
-    });
+    var data;
+
+    try {
+        data = await ses.sendEmail(params).promise();
+    }
+    catch (e) {
+        console.error(e);
+    }
+
+    console.log(data.MessageId);
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            "message": "sent successfully!",
+            "messageId": data.MessageId
+        }),
+    };
+
+    return response;
+}
